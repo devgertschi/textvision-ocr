71de0fc5beb82c66255dca0fd9aae28ec888527c
devgertschi <gerhard.kocher@hokify.com>
2025-05-16 16:50:36 +0200

    feat: favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
     head: {
       title: 'TextVision OCR - Extract Text from Images',
       link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap' }
